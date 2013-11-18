53b334975716fca9794e1ccc121840078f32263d
Cam Scotland <shnibles@gmail.com>
2013-11-18 00:42:18 -0800

    implemented cart model

--- js/cart.js
+++ js/cart.js
@@ -10,4 +10,26 @@
 
 function createCartModel(config) {
 
+	var model = createListModel(config);
+	
+	// loops over model's items array, summing price, and returning
+	// formatted (to two decimal places) total price of items.
+	model.getTotalPrice = function() {
+	
+		var index;
+		var totalPrice = 0;
+		for(index = 0; index < this.items.length; index++) {
+			totalPrice += this.items[index].price;
+		}
+		return totalPrice.toFixed(2);
+	}; //getTotalPrice()
+	
+	// returns the JSON representation (via browser's JavaScript 
+	// interpreter) of the model's items.
+	model.toJSON = function() {
+	
+		return JSON.stringify(this.items);
+	}; //toJSON()
+	
+	return model;
 } //createCartModel()
